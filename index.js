b5a10ae9f88b362c35cf5a34dce219c9f9038a51
Plinio Naves <pliniopjn@hotmail.com>
2019-03-11 16:15:32 -0300

    GraphQL Yoga: servidor e primeira Query

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
-async function main() {
-
-  await prisma.createUser({
-    name: 'Plínio Naves',
-    email: 'plinio@email.com',
-    password: '123456'
-  })
-
-  const users = await prisma.users()
-
-  console.log('Users: ', users)
-
+const resolvers = {
+  Query: {
+    user(parent, args, context, info) {
+      return prisma.user({ id: args.id })
+    }
+  }
 }
 
-main().catch(e => console.error(e))
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000...'))
