8ca502ae3b722117efd68e2ef95c263dbedc7d77
Plinio Naves <pliniopjn@hotmail.com>
2019-03-14 10:58:04 -0300

    Delegação de Schemas com Prisma Binding

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga')
+const Binding = require('prisma-binding')
 const { prisma } = require('./generated/prisma-client')
+
+const binding = new Binding.Prisma({
+  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+  endpoint: process.env.PRISMA_ENDPOINT
+})
 
 const resolvers = {
   Query: {
     user(parent, args, context, info) {
-      return prisma.user({ id: args.id })
+      /*return prisma.user({ id: args.id })
+        .then(user => {
+          console.log('User: ', user)
+          return user
+        })*/
+      return binding.query.user({ where: { id: args.id }}, info)
+        .then(user => {
+          console.log('User: ', user)
+          return user
+        })
     }
   }
 }
