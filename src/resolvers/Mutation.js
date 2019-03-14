8eec86f0cc277611d3b886fdaf2dd85cf5d9e27d
Plinio Naves <pliniopjn@hotmail.com>
2019-03-14 16:24:15 -0300

    Implementando resolver para a Mutation de login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,27 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+async function login (_, { email, password }, ctx, info) {
+
+  const user = await ctx.db.query.user({ where: { email } })
+  if (!user) {
+    throw new Error('Invalid credentials!')
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    throw new Error('Invalid credentials!')
+  }
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '2h' })
+
+  return {
+    token,
+    user
+  }
+
+}
+
 async function signup (_, args, ctx, info) {
 
   const password = await bcrypt.hash(args.password, 10)
@@ -18,5 +39,6 @@ async function signup (_, args, ctx, info) {
 }
 
 module.exports = {
+  login,
   signup
 }
