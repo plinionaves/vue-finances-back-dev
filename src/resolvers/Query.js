2c34dde60fe44a63ec3bf7fb98b47e66423f6f51
Plinio Naves <pliniopjn@hotmail.com>
2019-03-15 13:04:59 -0300

    Query para buscar lista de Categorias

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -19,6 +19,26 @@ function accounts (_, args, ctx, info) {
   }, info)
 }
 
+function categories (_, { operation }, ctx, info) {
+  const userId = getUserId(ctx)
+
+  let AND = [
+    {
+      OR: [
+        { user: { id: userId } },
+        { user: null }
+      ]
+    }
+  ]
+
+  AND = !operation ? AND : [ ...AND, { operation } ]
+
+  return ctx.db.query.categories({
+    where: { AND },
+    orderBy: 'description_ASC'
+  }, info)
+}
+
 function user (_, args, ctx, info) {
   const userId = getUserId(ctx)
   return ctx.db.query.user({ where: { id: userId }}, info)
@@ -26,5 +46,6 @@ function user (_, args, ctx, info) {
 
 module.exports = {
   accounts,
+  categories,
   user
 }
