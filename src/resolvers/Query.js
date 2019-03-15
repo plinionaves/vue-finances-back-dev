d7a0a2d0160b5ac7080bd567b14c5d89971b17a1
Plinio Naves <pliniopjn@hotmail.com>
2019-03-15 10:41:13 -0300

    Query para buscar lista de Contas

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,30 @@
 const { getUserId } = require('./../utils')
 
+function accounts (_, args, ctx, info) {
+  const userId = getUserId(ctx)
+  return ctx.db.query.accounts({
+    where: {
+      OR: [
+        {
+          user: {
+            id: userId
+          }
+        },
+        {
+          user: null
+        }
+      ]
+    },
+    orderBy: 'description_ASC'
+  }, info)
+}
+
 function user (_, args, ctx, info) {
   const userId = getUserId(ctx)
   return ctx.db.query.user({ where: { id: userId }}, info)
 }
 
 module.exports = {
+  accounts,
   user
 }
